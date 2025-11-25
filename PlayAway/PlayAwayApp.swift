77160cf77e054b75abe0f1b3e1c62eaca01eb67c
zhafirahkamila <zhafirahkamila.24@gmail.com>
2025-11-25 22:13:56 +0700

    Initial Commit

---
+++ PlayAway/PlayAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayAwayApp.swift
+//  PlayAway
+//
+//  Created by kamila on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PlayAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
